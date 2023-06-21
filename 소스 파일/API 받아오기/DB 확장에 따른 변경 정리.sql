f55964102b68225bb4a7b276183becaaa2003f88
909ma <sin00021@naver.com>
2023-06-21 14:21:38 +0900

    Update DB 확장에 따른 변경 정리.sql

--- 소스 파일/API 받아오기/DB 확장에 따른 변경 정리.sql
+++ 소스 파일/API 받아오기/DB 확장에 따른 변경 정리.sql
@@ -2,3 +2,11 @@
 CREATE VIEW HowMuchDailyMovie AS SELECT audiCnt, movieNm, salesAmt, targetDt FROM DailyMovie ORDER BY targetDt DESC LIMIT 3650;
 SELECT * FROM HowMuchDailyMovie;
 
+-- 상영작 통계 조회
+CREATE VIEW MovieChart AS 
+SELECT audiCnt, movieNm, salesAmt, targetDt
+FROM DailyMovie
+ORDER BY targetDt DESC
+LIMIT 1000;
+
+SELECT * FROM MovieChart ORDER BY audiCnt DESC;
