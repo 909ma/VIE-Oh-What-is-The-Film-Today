c33dc205d8c89d854139afe311546d71906dec66
909ma <sin00021@naver.com>
2023-06-27 14:37:45 +0900

    Update SQL소스.sql

--- 제출 파일 관리/SQL소스.sql
+++ 제출 파일 관리/SQL소스.sql
@@ -26,6 +26,15 @@ CREATE TABLE `myproject`.`dailymovie` (
   PRIMARY KEY (`movieCd`)
 );
 
+-- 공지사항 게시판
+CREATE TABLE IF NOT EXISTS notice (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  content TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
 -- 뷰
 CREATE VIEW HowMuchDailyMovie AS 
 SELECT audiCnt, movieNm, salesAmt, targetDt 
